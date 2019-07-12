be29a8bf5022145f981348e05c99666979b04697
chris <zhengjin3@jd.com>
2019-07-12 14:20:02 +0800

    add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { shallow, mount, render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('<App/>', () => {
+    it('enzyme', () => {
+        const component = render(<App/>);
+        expect(renderToJson(component)).toMatchSnapshot();
+    });
 });
