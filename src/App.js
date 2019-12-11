c9434682e0f45f0a3adf618052b3cf0b14cbd204
chris <zhengjin3@jd.com>
2019-12-11 14:23:48 +0800

    add tracker

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React,{Component} from 'react';
 
 import FastClick from 'fastclick';
@@ -5,16 +6,50 @@ import logo from './logo.svg';
 import './App.css';
 
 import {isTesting} from './utils/Utils';
+import * as Sentry from '@sentry/browser';
+import log from './bury/index'
 
+window.logger = new log({
+    mode:window.PAGE_CONFIG.env
+})
+
+// @logger.catchClass
 class App extends Component{
-  componentWillMount() {
+    constructor(props) {
+        super(props);
+
+        this.methodDoesNotExist = this.methodDoesNotExist.bind(this);
+
+        this.state = { eventId: null };
+    }
+    componentWillMount() {
       if(!isTesting()){
           FastClick.attach(document.body);
       }
-  }
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        Sentry.withScope((scope) => {
+            scope.setExtras(errorInfo);
+            const eventId = Sentry.captureException(error);
+            this.setState({eventId});
+        });
+    }
+
+    @logger.trackAction('test user action collection')
+    methodDoesNotExist(){
+        // Sentry.captureException(new Error("Something broke"));
+        throw new Error("Something broke")
+      return 1/0;
+    }
   render() {
     return (
         <div className="App">
+            <button onClick={this.methodDoesNotExist}>Break the world</button>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
+Sentry.init({dsn: "http://da505d2956e34e69a8e2d0535fce8c0b@localhost:9000/3", release: 'pro@1.0.1'});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
